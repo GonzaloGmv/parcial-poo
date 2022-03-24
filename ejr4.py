3b842a0fd5495d6ae769c7b86055a898e7ab6e7d
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2022-03-24 09:40:35 +0100

    vip

--- ejr4.py
+++ ejr4.py
@@ -53,5 +53,32 @@ class CuentaPlazoFijo(CuentaBancaria):
     def transferir(self):
         super().transferir()
 
-#ejercicio = CuentaPlazoFijo("123", "Gonzalo", "7/23", "1345", float(154.12), "7/22")
-#ejercicio.ingresar()
+class CuentaVip(CuentaBancaria):
+    def __init__(self, id, titular, apertura, numero_cuenta, saldo, saldo_negativo):
+        super().__init__(id, titular, apertura, numero_cuenta, saldo)
+        self.saldo_negativo = saldo_negativo
+
+    def retirar(self):
+        dinero = float(input("Escriba la cantidad que desea retirar: "))
+        if dinero > self.saldo + self.saldo_negativo:
+            print("Superaría el saldo negativo maximo establecido")
+        else:
+            self.saldo = self.saldo - dinero
+            print("Aquí está el dinero:", dinero, "euros")
+            print("Este es su saldo disponible:", self.saldo, "euros")
+
+    def ingresar(self):
+        super().ingresar()
+    
+    def transferir(self):
+        cuenta = input("Escriba el numero del cuenta al que quiere hacerle la transferencia: ")
+        dinero = float(input("Escriba la cantidad que desea transferir: "))
+        if dinero > self.saldo + self.saldo_negativo:
+            print("Superaría el saldo negativo maximo establecido")
+        else:
+            self.saldo = self.saldo - dinero
+            print("Se ha realizado la tranferencia de", dinero, "euros, a la cuenta", cuenta)
+            print("Su saldo actual es", self.saldo)
+
+ejercicio = CuentaVip("123", "Gonzalo", "7/23", "1345", float(20), float(20))
+ejercicio.retirar()

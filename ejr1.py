c791c12cf62068b8a558eed21fd0e70ff1b98d2b
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2022-03-24 10:07:31 +0100

    no se hacer get y set

--- ejr1.py
+++ ejr1.py
@@ -1,11 +1,12 @@
 class libro():
-    def __init__(self, autor, titulo, editorial):
+    def __init__(self, autor, titulo, editorial, isbn):
         self.autor = autor
         self.titulo = titulo
         self.editorial = editorial
+        self.isbn = isbn
     
     def libro(self):
-        print("Libro:", self.titulo, "de", self.autor, "de la editorial", self.editorial)
-
-ejercicio = libro("Cervantes", "El Quijote", "Anaya")
+        print("Libro:", self.titulo, "de", self.autor, "con ISBN", self.isbn, "de la editorial", self.editorial)
+ 
+ejercicio = libro("Cervantes", "El Quijote", "Editorial", "123456789")
 ejercicio.libro()
